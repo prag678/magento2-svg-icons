40c228e5acd95263dcac82bb4ea3aa2b2aefae37
Ubuntu <ubuntu@mage1.bbrouter>
2026-01-22 13:45:13 +0530

    Initial commit: Magento 2 SvgIcons module

---
+++ Block/Index.php
@@ -0,0 +1,6 @@
+<?php
+namespace Magediary\SvgIcons\Block;
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ ViewModel/SvgIconsCustom.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magediary\SvgIcons\ViewModel;
+
+use Hyva\Theme\ViewModel\SvgIcons;
+
+class SvgIconsCustom extends SvgIcons
+{
+
+}
